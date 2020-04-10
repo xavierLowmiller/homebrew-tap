641e2ba10a3f987b46067468ced4a13f9bce6e00
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-04-10 08:57:38 +0200

    Rename formula

--- axml-to-xml.rb
+++ axml-to-xml.rb
@@ -1,6 +1,6 @@
 class AxmlToXml < Formula
   desc "A converter for android xml to regular xml"
-  homepage "xavierlowmiller.github.io"
+  homepage "https://github.com/xavierLowmiller/swift-axml-converter"
   url "https://github.com/xavierLowmiller/swift-axml-converter/releases/download/1.0.0/axml-to-xml-macOS-latest.zip"
   sha256 "39e66d66c4a25fa9dc3288c7bcaa34f025d2874bcbdad08e723951cf5d0fcf02"
 
