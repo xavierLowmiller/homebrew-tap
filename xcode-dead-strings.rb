05e5b06a421e9950f3271d7ad77e1a097a334715
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-04-18 17:52:33 +0200

    Update xcode-dead-strings.rb

--- xcode-dead-strings.rb
+++ xcode-dead-strings.rb
@@ -4,8 +4,8 @@
 class XcodeDeadStrings < Formula
   desc "Tool to find dead localized Strings in Xcode projects"
   homepage "https://github.com/xavierLowmiller/xcode-dead-strings"
-  url "https://github.com/xavierLowmiller/xcode-dead-strings/releases/download/1.0.0/xcode-dead-strings.zip"
-  sha256 "3b05fcbb9ecad0eca05bf4607807c1e2a3956a849c255d91a84dafd9fe673430"
+  url "https://github.com/xavierLowmiller/xcode-dead-strings/releases/download/1.0.1/xcode-dead-strings.zip"
+  sha256 "afbcc4e2f1583737abd01903b3ad53e1651bb379563578915ad5576df1ab9f5d"
 
   def install
     bin.install "xcode-dead-strings"
