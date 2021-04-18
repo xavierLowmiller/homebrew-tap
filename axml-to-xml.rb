d3ca4794629f223b76d3771f4ac416738362a836
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-04-18 08:33:32 +0200

    Update style

--- axml-to-xml.rb
+++ axml-to-xml.rb
@@ -1,5 +1,8 @@
+# typed: false
+# frozen_string_literal: true
+
 class AxmlToXml < Formula
-  desc "A converter for android xml to regular xml"
+  desc "Converter for android xml to regular xml"
   homepage "https://github.com/xavierLowmiller/swift-axml-converter"
   url "https://github.com/xavierLowmiller/swift-axml-converter/releases/download/1.0.0/axml-to-xml-macOS-latest.zip"
   sha256 "39e66d66c4a25fa9dc3288c7bcaa34f025d2874bcbdad08e723951cf5d0fcf02"
@@ -7,5 +10,4 @@ class AxmlToXml < Formula
   def install
     bin.install "axml-to-xml"
   end
-
 end
--- xcode-dead-strings.rb
+++ xcode-dead-strings.rb
@@ -1,5 +1,8 @@
+# typed: false
+# frozen_string_literal: true
+
 class XcodeDeadStrings < Formula
-  desc "A tool to find dead localized Strings in Xcode projects"
+  desc "Tool to find dead localized Strings in Xcode projects"
   homepage "https://github.com/xavierLowmiller/xcode-dead-strings"
   url "https://github.com/xavierLowmiller/xcode-dead-strings/releases/download/1.0.0/xcode-dead-strings.zip"
   sha256 "ee7636d516d8caa865b7d4d9a0976fd20e14813bb33bd537591421a1caa44f68"
@@ -7,5 +10,4 @@ class XcodeDeadStrings < Formula
   def install
     bin.install "xcode-dead-strings"
   end
-
 end
