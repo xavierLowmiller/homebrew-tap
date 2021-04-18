56f772756ef4ee8afa7aefb09d6513033e3588d9
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-04-18 08:41:31 +0200

    Use correct shasum

--- xcode-dead-strings.rb
+++ xcode-dead-strings.rb
@@ -5,7 +5,7 @@ class XcodeDeadStrings < Formula
   desc "Tool to find dead localized Strings in Xcode projects"
   homepage "https://github.com/xavierLowmiller/xcode-dead-strings"
   url "https://github.com/xavierLowmiller/xcode-dead-strings/releases/download/1.0.0/xcode-dead-strings.zip"
-  sha256 "ee7636d516d8caa865b7d4d9a0976fd20e14813bb33bd537591421a1caa44f68"
+  sha256 "3b05fcbb9ecad0eca05bf4607807c1e2a3956a849c255d91a84dafd9fe673430"
 
   def install
     bin.install "xcode-dead-strings"
